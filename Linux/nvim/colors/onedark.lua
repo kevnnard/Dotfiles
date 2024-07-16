7f208cf9ed27372baa543f1abee26a6adc52c700
KevnnardFedora <londonokevin9@gmail.com>
2024-07-16 18:49:40 -0500

    feat: add new dependecies to lazygit

--- Linux/nvim/colors/onedark.lua
+++ Linux/nvim/colors/onedark.lua
@@ -1,6 +1,8 @@
 for k in pairs(package.loaded) do
-    if k:match(".*onedark.*") then package.loaded[k] = nil end
+  if k:match(".*onedark.*") then
+    package.loaded[k] = nil
+  end
 end
 
-require('onedark').setup()
-require('onedark').colorscheme()
+require("onedark").setup()
+require("onedark").colorscheme()
--- Linux/nvim/lua/config/keymaps.lua
+++ Linux/nvim/lua/config/keymaps.lua
@@ -4,19 +4,54 @@
 
 -- VERSION PACKAGES KEYMAPS
 -- Show dependency versions
-vim.keymap.set({ "n" }, "<LEADER>ns", require("package-info").show, { silent = true, noremap = true })
+vim.keymap.set(
+  { "n" },
+  "<LEADER>ns",
+  require("package-info").show,
+  { silent = true, noremap = true, desc = "Show dependency versions" }
+)
 -- Hide dependency versions
-vim.keymap.set({ "n" }, "<LEADER>nc", require("package-info").hide, { silent = true, noremap = true })
+vim.keymap.set(
+  { "n" },
+  "<LEADER>nc",
+  require("package-info").hide,
+  { silent = true, noremap = true, desc = "Hide dependency versions" }
+)
 -- Toggle dependency versions
-vim.keymap.set({ "n" }, "<LEADER>nt", require("package-info").toggle, { silent = true, noremap = true })
+vim.keymap.set(
+  { "n" },
+  "<LEADER>nt",
+  require("package-info").toggle,
+  { silent = true, noremap = true, desc = "Toggle dependency versions" }
+)
 -- Update dependency on the line
-vim.keymap.set({ "n" }, "<LEADER>nu", require("package-info").update, { silent = true, noremap = true })
+vim.keymap.set(
+  { "n" },
+  "<LEADER>nu",
+  require("package-info").update,
+  { silent = true, noremap = true, desc = "Update dependency on the line" }
+)
 -- Delete dependency on the line
-vim.keymap.set({ "n" }, "<LEADER>nd", require("package-info").delete, { silent = true, noremap = true })
+vim.keymap.set(
+  { "n" },
+  "<LEADER>nd",
+  require("package-info").delete,
+  { silent = true, noremap = true, desc = "Delete dependency on the line" }
+)
 -- Install a new dependency
-vim.keymap.set({ "n" }, "<LEADER>ni", require("package-info").install, { silent = true, noremap = true })
+vim.keymap.set(
+  { "n" },
+  "<LEADER>ni",
+  require("package-info").install,
+  { silent = true, noremap = true, desc = "Install a new dependency" }
+)
 -- Install a different dependency version
-vim.keymap.set({ "n" }, "<LEADER>np", require("package-info").change_version, { silent = true, noremap = true })
+vim.keymap.set(
+  { "n" },
+  "<LEADER>np",
+  require("package-info").change_version,
+  { silent = true, noremap = true, desc = "Install a different dependency version" }
+)
 
 -----------------------------------------------------------------------------------------------------------------------
 ------------------------------------------------------------------------------------------------------------------------

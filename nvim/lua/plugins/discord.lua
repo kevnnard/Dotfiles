6f2111242f66bf76b918bab48230f4737583a147
Kevnnard MacOs <londonokevin9@gmail.com>
2024-07-25 22:01:10 -0500

    feat: add new plugins and refactor code to improve performance

--- nvim/lua/plugins/discord.lua
+++ nvim/lua/plugins/discord.lua
@@ -3,12 +3,29 @@ return {
   build = "./build",
   event = "VeryLazy",
   opts = {
+    timer = {
+      interval = 1000,
+    },
+    editor = {
+      tooltip = "What're u see bro?",
+    },
     display = {
-      show_repository = false,
       show_time = false,
+      show_repository = false,
+      workspace_blacklist = {
+        "V3",
+      },
     },
     lsp = {
       show_problem_count = true,
+      severety = 4,
+    },
+    idle = {
+      timeout = 0,
+      text = "Thinking...",
+    },
+    text = {
+      editing = "Working on {}",
     },
   },
 }

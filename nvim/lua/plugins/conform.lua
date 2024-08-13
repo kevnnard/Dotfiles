7930e61524aefde463e15a4b2fc90b3ac0d437e2
KevnnardFedora <londonokevin9@gmail.com>
2024-08-13 00:30:23 -0500

    feat: add new plugins and refactor code to improve performance

--- nvim/lua/plugins/conform.lua
+++ nvim/lua/plugins/conform.lua
@@ -6,7 +6,7 @@ return {
       formatters = {
         biome = {
           command = "biome",
-          cwd = require("conform.util").root_file({ ".biome.json" }),
+          cwd = require("conform.util").root_file({ "biome.json", "biome.jsonc" }),
           formatStdin = true,
         },
         prettier = {
@@ -18,8 +18,29 @@ return {
       formatters_by_ft = {
         lua = { "stylua" },
         sh = { "shellcheck" },
+        json = function(bufnr)
+          if require("conform").get_formatter_info("jsonls", bufnr).available then
+            return { "jsonls" }
+          else
+            return { "prettier" }
+          end
+        end,
         astro = function(bufnr)
           if require("conform").get_formatter_info("biome", bufnr).available then
+            return { "biome" }
+          else
+            return { "prettier" }
+          end
+        end,
+        typescript = function(bufno)
+          if require("conform").get_formatter_info("biome", bufno).available then
+            return { "biome" }
+          else
+            return { "prettier" }
+          end
+        end,
+        javascript = function(bufno)
+          if require("conform").get_formatter_info("biome", bufno).available then
             return { "biome" }
           else
             return { "prettier" }
@@ -31,6 +52,12 @@ return {
       if LazyVim.has_extra("formatting.prettier") then
         opts.formatters_by_ft = opts.formatters_by_ft or {}
         opts.formatters_by_ft.astro = { "prettier" }
+      end
+      if LazyVim.has_extra("formatting.biome") then
+        opts.formatters_by_ft = opts.formatters_by_ft or {}
+        opts.formatters_by_ft.astro = { "biome" }
+        opts.formatters_by_ft.typescript = { "biome" }
+        opts.formatters_by_ft.javascript = { "biome" }
       end
     end,
   },

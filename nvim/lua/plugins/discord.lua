7fe4d1f114999012f998261ca6fc2d3a4f17be61
Kevnnard MacOS <londonokevin9@gmail.com>
2024-11-21 23:33:05 -0500

    feat: add new config

--- nvim/lua/plugins/discord.lua
+++ nvim/lua/plugins/discord.lua
@@ -1,36 +1,60 @@
+-- return {
+--   "vyfor/cord.nvim",
+--   build = ".\\build",
+--   event = "VeryLazy",
+--   opts = {
+--     timer = {
+--       interval = 1000,
+--       reset_on_idle = true,
+--     },
+--     editor = {
+--       tooltip = "What're u see bro?",
+--     },
+--     display = {
+--       show_time = false,
+--       show_repository = false,
+--       workspace_blacklist = {
+--         "V3",
+--         "DOTFILES",
+--         "BACKEND",
+--         "FRONTEND",
+--       },
+--     },
+--     lsp = {
+--       show_problem_count = true,
+--       severety = 4,
+--     },
+--     idle = {
+--       disable_on_focus = true,
+--       timeout = 0,
+--       text = "Thinking...",
+--     },
+--     text = {
+--       editing = "Working on {}",
+--     },
+--   },
+-- }
 return {
-  enabled = false,
-  "vyfor/cord.nvim",
-  build = "./build",
+  "IogaMaster/neocord",
   event = "VeryLazy",
   opts = {
-    timer = {
-      interval = 1000,
+    logo = "auto",
+    logo_tooltip = "What're u see bro?",
+    log_level = "error",
+    blacklist = {
+      "v3",
+      "Dotfiles",
+      "Backend",
+      "Frontend",
     },
-    editor = {
-      tooltip = "What're u see bro?",
+    file_assets = {
+      lua = { "Luaa", "lua" },
+      vim = { "Vim", "vim" },
+      nvim = { "Nvim", "nvim" },
+      [".Browsing"] = { "Test", "lua" },
     },
-    display = {
-      show_time = false,
-      show_repository = false,
-      workspace_blacklist = {
-        "V3",
-        "DOTFILES",
-        "BACKEND",
-        "FRONTEND",
-      },
-    },
-    lsp = {
-      show_problem_count = true,
-      severety = 4,
-    },
-    idle = {
-      disable_on_focus = true,
-      timeout = 0,
-      text = "Thinking...",
-    },
-    text = {
-      editing = "Working on {}",
-    },
+
+    show_time = true,
+    global_timer = true,
   },
 }
